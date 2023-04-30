1a382e4e8835a7891091c9e10f0f1d7e30a351ae
NeerajReddy21 <94236178+NeerajReddy21@users.noreply.github.com>
2023-04-30 16:31:42 +0530

    adding my appn

---
+++ src/main/java/com/web/service/BankServiceImp.java
@@ -0,0 +1,162 @@
+package com.web.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.web.model.Bank;
+import com.web.model.BankT;
+import com.web.model.BankTemp;
+import com.web.repo.BankRepo;
+
+@Service
+public class BankServiceImp implements BankService {
+	@Autowired
+	private BankRepo repo;
+
+	@Override
+	public Bank customerReg(BankTemp banktemp) {		
+		Bank bank=new Bank();
+		bank.setAcno(banktemp.getAcno());	
+		bank.setName(banktemp.getName());
+		if(banktemp.getPassword().equals(banktemp.getConfirmpd()))
+		{
+			bank.setPassword(banktemp.getConfirmpd());
+		
+		bank.setAddress(banktemp.getAddress());
+		bank.setAmount(banktemp.getAmount());
+		bank.setMobile(banktemp.getMobile());
+		repo.save(bank);
+		}
+		return bank;
+		
+		
+	}
+
+	@Override
+	public Bank getCustmor(Bank bank) {
+		
+		
+		
+		Bank bank1=repo.findById(bank.getAcno()).get();
+		if(bank.getAcno()==bank1.getAcno()&&bank.getName().equalsIgnoreCase(bank1.getName())&&bank.getPassword().equals(bank1.getPassword()))
+		{
+		return bank1;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	@Override
+	public Bank depost(Bank bank) {
+
+		Bank bank1=repo.findById(bank.getAcno()).get();
+		
+		if(bank.getAcno()==bank1.getAcno()&&bank.getName().equalsIgnoreCase(bank1.getName())&&bank.getPassword().equals(bank1.getPassword()))
+		{
+			bank1.setAmount(bank1.getAmount()+bank.getAmount());
+			repo.save(bank1);	
+
+		return bank1;
+		}
+		else
+		{
+			return null;
+		}		
+	}
+
+	@Override
+	public Bank withdrawAmount(Bank bank) {
+		
+Bank bank1=repo.findById(bank.getAcno()).get();
+		
+		if(bank.getAcno()==bank1.getAcno()&&bank.getName().equalsIgnoreCase(bank1.getName())&&bank.getPassword().equals(bank1.getPassword()))
+		{
+			bank1.setAmount(bank1.getAmount()-bank.getAmount());
+			repo.save(bank1);	
+
+		return bank1;
+		}
+		else
+		{
+			return null;
+		}		
+
+		
+	}
+
+	@Override
+	public List<Bank> transferAmt(BankT bank) {
+		Bank b1=repo.findById(bank.getAcno()).get();
+		Bank b2= repo.findById(bank.getTacno()).get();
+		
+		
+		if(b1.getAcno()==bank.getAcno()&&b1.getName().equals(bank.getName())&&b1.getPassword().equals(bank.getPassword()))
+		{
+			
+			b1.setAmount(b1.getAmount()-bank.getAmount());
+			repo.save(b1);
+			
+			b2.setAmount(b2.getAmount()+bank.getAmount());
+			repo.save(b2);
+			
+			List<Bank>list=List.of(b1,b2);
+			return list;
+		}	
+
+		
+		return null;
+	}
+
+	@Override
+	public void deleteAccount(Bank bank) {
+		Bank b=repo.findById(bank.getAcno()).get();
+		if(b.getAcno()==bank.getAcno()&&b.getName().equals(bank.getName())&&b.getPassword().equals(bank.getPassword()))
+		{
+			repo.deleteById(bank.getAcno());
+		}
+	}
+
+	@Override
+	public Bank customerDup(BankTemp banktemp) {
+		Bank b=null;
+		try {
+		b = repo.findById(banktemp.getAcno()).get();
+		return b;
+		}
+		catch (Exception e) {
+				return b;
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
